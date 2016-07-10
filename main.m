1f8562a402a6d9ada7a5df09643e5c0cfb2acfc3
Tong Chen <Tong Chen>
2016-07-10 18:47:57 +0100

    play more graphics plot functions

--- main.m
+++ main.m
@@ -6,7 +6,17 @@ c++;
 ++c;
 
 x = [10:30, 15:20, 20:1, 50:2];
+y = -10:10;
 
 subplot (221);
   plot (x);
   title ('test plot');
+subplot (222);
+  plot (y, sqrt(y));
+  title ('test plot 2');
+subplot (223);
+  plot (y, log2(y));
+  title ('test plot 2');   
+subplot (224);
+  plot (y, power(y, 2));
+  title ('test plot 2');    
