41eb22831e0d99483643f878d7138b420e7ad58f
Tong <chentongtc@gmail.com>
2016-07-16 11:59:27 +0100

    more plot

--- main.m
+++ main.m
@@ -48,8 +48,8 @@ subplot (5,5,11);
   plot (y, power(y, 6));
   title ('y ^ 6');  
 subplot (5,5,12);
-  plot (y, power(y, 7));
-  title ('y ^ 7');  
+  plot (y, power(y, 7), y, power(y, 8), y, power(y, 9), y, power(y, 10));
+  title ('y ^ 7, y ^ 8, y ^ 9, y ^ 10');  
   
 subplot (5,5,13);
   plot (y, log2(y));
@@ -64,6 +64,16 @@ subplot (5,5,15);
 subplot (5,5,16);
   treeplot ([2 4 2 0 6 4 6]);
   title ('treeplot [2 4 2 0 6 4 6]');   
+  
+subplot (5,5,17);
+x = 0:0.01:2;                 %  generate the x-vector
+	y = 5*x.*exp(-3*x);           %  and the "true" function, y
+	yn = y + 0.02*randn(size(x)); %  Create a noisy version of y
+	plot(x,y,'-',x,yn,'ro');      %  Plot the true and the noisy
+
+	xlabel('x (arbitrary units)');       %  add axis labels and plot title
+	ylabel('y (arbitrary units)');
+	title('Plot of y = 5*x*exp(-3*x) + noise');
  
 #subplot (5,5,17);
 #  plotmatrix (randn (100, 3), 'g+')
